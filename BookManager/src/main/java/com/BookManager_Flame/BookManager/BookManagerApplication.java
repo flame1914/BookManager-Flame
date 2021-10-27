e4f08d5595571549d026af839a2cc2a7db15afe5
“flame1914” <644067852@qq.com>
2021-10-27 11:30:10 +0800

    first commit

---
+++ BookManager/src/main/java/com/BookManager_Flame/BookManager/BookManagerApplication.java
@@ -0,0 +1,13 @@
+package com.BookManager_Flame.BookManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManagerApplication.class, args);
+	}
+
+}
